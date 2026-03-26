17c5fc5b807aef3206fa323ebe317b75ce3c246d
Vasu Gupta <vasupankajdbd@gmail.com>
2026-03-26 10:48:56 +0530

    added vercel url for cors resolve

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://chattrix-nmlf.vercel.app'],
     credentials: true
 }));
 
